b1b5d8711b5dde8e596203785ecb2ba5703be796
MihaiElisei <mihai.m.elisei@gmail.com>
2022-12-05 18:30:17 +0000

    update settings

--- HR/settings.py
+++ HR/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['human-resources.onrender.com', 'localhost']
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
