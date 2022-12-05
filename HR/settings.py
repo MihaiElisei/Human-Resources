e6e409e05ac3475cefef269a6962c83ea1d3d1e6
MihaiElisei <mihai.m.elisei@gmail.com>
2022-12-05 19:15:56 +0000

    debug true

--- HR/settings.py
+++ HR/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
